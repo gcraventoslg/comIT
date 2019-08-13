b1a77e05f1b1ee35258722fb517507cb9e5f84ff
Gloria <gloriaraventoslg@gmail>
2019-08-13 16:44:17 -0500

    Added js Exercises 52-57

---
+++ js/index55.js
@@ -0,0 +1,7 @@
+const firstNumber  = 10;
+//const secondNumber  = 5;
+const secondNumber  = 25;
+
+let message = ( firstNumber > secondNumber ) ? `Number ${firstNumber} is bigger than ${secondNumber}` : `Number ${secondNumber} is bigger than ${firstNumber}`;
+
+console.log(message);
---
+++ js/index56.js
@@ -0,0 +1,5 @@
+const number  = 13;
+
+let message = ( number % 2 === 0 ) ? `${ number } is an even number` : `${ number } is an odd number`;
+
+console.log(message);
