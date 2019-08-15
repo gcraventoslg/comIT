99b29bb90342cd8c4e119385434ee88f9819f8ce
Gloria <gloriaraventoslg@gmail>
2019-08-15 10:51:49 -0500

    Added exercises 64-65 ja

---
+++ js/index64.js
@@ -0,0 +1,10 @@
+
+let name = null;
+name = 'Gloria';
+
+if ( name && name !== null ) {    
+    console.log(` Your name is ${name}`);
+} else {
+    console.log('Please input a valid name');
+}
+
---
+++ js/index65.js
@@ -0,0 +1,10 @@
+
+let balance  = 0;
+balance = 100;
+
+if ( balance && balance > 0 ) {    
+    console.log(` Your balance is: ${balance}`);
+} else {
+    console.log('Sorry, no money in your account');
+}
+
