6c415e27dd44f98c145d577473785878377bdd95
Gloria <gloriaraventoslg@gmail>
2019-08-15 18:39:37 -0500

    Fixed 69 js exercise

--- js/index69.js
+++ js/index69.js
@@ -2,10 +2,10 @@
 let number = 0;
 let sumResult = 0;
 
-while ( number < 1001 ) {
-    console.log(`${sumResult} + ${number} = ${sumResult+number}`);    
+while ( number < 1001 ) {     
     sumResult += number;
+    console.log(`Parcial result = ${sumResult}`);   
     number++;
 }
 
-console.log('Partial result:',sumResult);
+//console.log('Partial result:',sumResult);
