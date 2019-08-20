a8ac47ea00e2e24ae116880c1faa74c4c92e35a8
Gloria <gloriaraventoslg@gmail>
2019-08-20 12:45:29 -0500

    Added other condition

--- js/index58.js
+++ js/index58.js
@@ -8,8 +8,10 @@ if ( hour >= 5 && hour < 12 ) {
     message = 'Good Afternoon';
 } else if ( hour >= 18 && hour < 22 ) {
     message = 'Good evening';
-} else {
+} else if ( (hour >= 22 && hour <=24) && ( hour >= 1 && hour < 5) ) {
     message = 'Good Night';
+} else {
+    message = 'Enter a time between 1 and 24';
 }
 
 console.log(message);
