84b52798543bbbd27d89d993beac5122ec079879
Gloria <gloriaraventoslg@gmail>
2019-08-23 14:02:53 -0500

    Updated 154 js exercise

--- js/index154.js
+++ js/index154.js
@@ -4,20 +4,36 @@ const player = {
                 energy: 100,
                 lives: 3,
                 loseEnergy: function (damage) {
-                    this.energy - damage > 0 ? this.energy -= damage : this.loseLife();
+                    this.energy-=damage;
+                    if (!this.hasEnergy()) {
+                        this.loseLife(); 
+                    }                    
                 },
                 recoverEnergy: function (damage) {
                     if (this.energy + damage <= 100) 
-                        this.energy += damage;
-                                       
+                        this.energy += damage;                                       
                 },
-                loseLife: function () {
-                    this.lives--;
+                loseLife: function () {                    
+                    this.lives--;                    
+                    if (!this.verifyLive()){ 
+                        console.log('Game Over!');                                            
+                    }
                 },
                 recoverLife: function () {
-                    if (this.lives + 1 <= 99) 
-                        this.lives++;                                           
+                    if (this.lives + 1 <= 99){
+                        if (this.verifyLive() && this.hasEnergy()) {
+                            this.lives++;                                               
+                        }else{
+                            console.log('Game Over!');
+                        }                        
+                    }                         
                 },
+                verifyLive: function () {
+                    return this.lives > 0 ? true : false;
+                },
+                hasEnergy: function () {
+                    return this.energy >0 ? true : false;
+                }
 }
 player.name = 'Gloria';
 player.loseLife();
