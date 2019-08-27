fccf07498527d02c6b7808616943fddee9cd1a08
Gloria <gloriaraventoslg@gmail>
2019-08-27 18:42:48 -0500

    Fixed 130 js exercise

--- js/index130.js
+++ js/index130.js
@@ -1,11 +1,17 @@
-
-const mutants  = ['Professor X','Cyclops','Iceman','Angel','Beast','Phoenix'];
+const mutants = [
+  "Professor X",
+  "Cyclops",
+  "Iceman",
+  "Angel",
+  "Beast",
+  "Phoenix"
+];
 const mutantsSorts = mutants.sort();
-const mutantsReverse = mutantsSorts.reverse();
 
-console.log('Original mutants list:', mutants);
-console.log('Sorted mutants list:', mutantsSorts);
-console.log('Reversed mutants list:', mutantsReverse);
-console.log('Mutants name separated by * :',mutantsReverse.join(' * '));
+const newMutants = [...mutants];
+const mutantsReverse = newMutants.reverse();
 
-
+console.log("Original mutants list:", mutants);
+console.log("Sorted mutants list:", mutantsSorts);
+console.log("Reversed mutants list:", mutantsReverse);
+console.log("Mutants name separated by * :", mutantsReverse.join(" * "));
