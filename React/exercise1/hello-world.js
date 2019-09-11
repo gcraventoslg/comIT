881ba5d908aaf739972f2986d44699ff0cbd7778
Gloria <gloriaraventoslg@gmail>
2019-09-10 23:27:21 -0500

    Added react first projects

---
+++ React/exercise1/hello-world.js
@@ -0,0 +1,10 @@
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage name="Gloria" />,
+  document.getElementById("my-app")
+);
---
+++ React/react-tutorial/src/Table.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    );
+  }
+}
+export default Table;
